1f96e5bb617ca5093cea45efe32355edc2460ec4
Abby <i@abby.md>
2023-07-27 17:55:08 +0900

    remove strings dependency

--- base94.go
+++ base94.go
@@ -2,47 +2,41 @@
 
 package base94
 
-import "strings"
-
 const minChar = 33
 
 // Encode encodes the given binary data using the custom character set.
 func Encode(data []byte) string {
-	var encoded strings.Builder
+	// Calculate the maximum length of the encoded string.
+	maxEncodedLen := (len(data)*8 + 5) / 6 // Ceiling of (len(data) * 8 / 6)
+
+	// Create a fixed-length byte slice to hold the encoded characters.
+	encoded := make([]byte, maxEncodedLen)
 
 	var buffer int         // Temporary buffer to hold bits from the input data.
 	var bitsRemaining uint // Number of bits remaining in the buffer to form the next character.
+	var index int          // Index to track the current position in the encoded slice.
 
 	for _, b := range data {
-		// Append the current byte to the buffer and shift it to the left to make room for the next byte.
 		buffer |= int(b) << bitsRemaining
 		bitsRemaining += 8
 
-		// Form characters from the buffer as long as we have enough bits (6 or more) to form one.
 		for bitsRemaining >= 6 {
-			// Extract the lower 6 bits of the buffer (63 in binary is 111111) to get the character index.
 			charIndex := buffer & 63
+			encoded[index] = byte(charIndex) + minChar
 
-			// Add the minimum character value to the character index to get the actual character value.
-			encoded.WriteByte(byte(charIndex) + minChar)
-
-			// Shift the buffer to the right to remove the 6 bits that were just used to form the character.
+			index++
 			buffer >>= 6
-			// Reduce the count of bits remaining in the buffer.
 			bitsRemaining -= 6
 		}
 	}
 
-	// If there are remaining bits in the buffer (less than 6), form a character from them.
 	if bitsRemaining > 0 {
-		// Extract the remaining bits (less than 6) to get the character index.
 		charIndex := buffer & 63
-
-		// Add the minimum character value to the character index to get the actual character value.
-		encoded.WriteByte(byte(charIndex) + minChar)
+		encoded[index] = byte(charIndex) + minChar
+		index++
 	}
 
-	return encoded.String()
+	return string(encoded[:index])
 }
 
 // Decode decodes the given encoded data using the custom character set and returns binary data.
